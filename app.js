35ccf440cd1fefa6a188a2e7c276f21d867abd4d
Felix Klauke <info@felix-klauke.de>
2018-06-24 22:21:39 +0200

    Add twig template and start layout.
    Signed-off-by: Felix Klauke <info@felix-klauke.de>

--- app.js
+++ app.js
@@ -1,8 +1,10 @@
 let express = require('express');
 let app = express();
 
+app.set('view engine', 'twig');
+
 app.get('/', function (request, response) {
-    response.send('Hello World');
+    response.render('index.html.twig', {'name': 'John Doe'})
 });
 
 let server = app.listen(8081, function () {
