be60cf9dc8c8409e426026ee2b21eb2ffa9b5e76
Felix Klauke <info@felix-klauke.de>
2018-06-25 08:33:17 +0200

    Add sass middleware and include css style sheets while ignoring resulting build files and commenting app.js structure.
    Signed-off-by: Felix Klauke <info@felix-klauke.de>

--- app.js
+++ app.js
@@ -1,12 +1,41 @@
+/**
+ * Require needed dependencies and resources.
+ */
 let express = require('express');
+let sassMiddleware = require('node-sass-middleware');
+let path = require('path');
 let app = express();
 
+/**
+ * Set templating engine to twig.
+ */
 app.set('view engine', 'twig');
 
+/**
+ * Configure app to use middleware.
+ */
+app.use(sassMiddleware({
+    src: path.join(__dirname, 'assets'),
+    dest: path.join(__dirname, 'build'),
+    outputStyle: 'compressed',
+    prefix: '/vendor'
+}));
+
+/**
+ * Expose public directory.
+ */
+app.use('/public', express.static(path.join(__dirname, 'build')));
+
+/**
+ * Routing configuration.
+ */
 app.get('/', function (request, response) {
     response.render('index.html.twig', {'name': 'John Doe'})
 });
 
+/**
+ * Let teh server listen for requests on a certain port.
+ */
 let server = app.listen(8081, function () {
     console.log('Valentine app listening on port ' + server.address().port);
 });
