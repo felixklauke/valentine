058762d12ea4483dafe037f96ed6d204d1dced30
Felix Klauke <info@felix-klauke.de>
2018-06-26 09:21:04 +0200

    Add login via Google, change default listen port to 3001, add environment variable based configuration for OAuth2 configuration for google login based on passport.js.
    Signed-off-by: Felix Klauke <info@felix-klauke.de>
    
    Add login via Google, change default listen port to 3001, add environ…
    
    ment variable based configuration for OAuth2 configuration for google login based on passport.js

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # valentine
 Browser based chess platform for the ultimate modern fashioned way to play chess online.
+
+# Configuration
+As we use dotenv for environment variables you should create a .env in the source root with the following content:
+```
+GOOGLE_OAUTH_CLIENT_ID=<Your client id>
+GOOGLE_OAUTH_CLIENT_SECRET=<Your client secret>
--- app.js
+++ app.js
@@ -3,9 +3,16 @@
  */
 let express = require('express');
 let sassMiddleware = require('node-sass-middleware');
+let dotenv = require('dotenv');
 let passport = require('passport');
+let passportGoogleOAuth = require('passport-google-oauth20');
 let path = require('path');
 let app = express();
+
+/**
+ * Environment variables
+ */
+dotenv.config();
 
 /**
  * Set templating engine to twig.
@@ -23,6 +30,20 @@ app.use(sassMiddleware({
 }));
 
 /**
+ * Authentication strategy.
+ */
+let GoogleStrategy = passportGoogleOAuth.Strategy;
+passport.use(new GoogleStrategy({
+        clientID: process.env.GOOGLE_OAUTH_CLIENT_ID,
+        clientSecret: process.env.GOOGLE_OAUTH_CLIENT_SECRET,
+        callbackURL: "https://d3adspace.de/auth/google/callback"
+    },
+    function (accessToken, refreshToken, profile, callback) {
+
+    }
+));
+
+/**
  * Expose directories and library access.
  */
 app.use('/vendor', express.static(path.join(__dirname, 'public/build')));
@@ -36,9 +57,14 @@ app.use('/bootstrap', express.static(path.join(__dirname, 'node_modules/bootstra
 const dashboardController = require('./routes/dashboardController');
 app.use('/', dashboardController);
 
+app.get('/auth/google', passport.authenticate('google', {scope: ['profile']}));
+app.get('/auth/google/callback', passport.authenticate('google', {failureRedirect: '/login'}), function (req, res) {
+    res.redirect('/');
+});
+
 /**
  * Let teh server listen for requests on a certain port.
  */
-let server = app.listen(8081, function () {
+let server = app.listen(3001, function () {
     console.log('Valentine app listening on port ' + server.address().port);
 });
